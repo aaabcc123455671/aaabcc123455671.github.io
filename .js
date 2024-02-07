87365034b20401a61c71da82b416c71543b9c7eb
aaabcc123455671 <159030704+aaabcc123455671@users.noreply.github.com>
2024-02-07 22:35:03 +0800

    Delete .js

--- .js
+++
@@ -1,5 +0,0 @@
-const btn=document.querySelector(".button");
-const feedback=document.querySelector(".feedback-container");
-btn.addEventListener("click",()=>{
-  feedback.innerHTML=<h1>Thank You For Your Feedback</h1>;
-});
